828895c762e54207808c6c23f358ff01aeffe365
gus <19171@wc.school.nz>
2023-03-31 14:09:04 +1300

    Initial Commit

---
+++ TeReoDictionary/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect,request,session
+import sqlite3
+from sqlite3 import Error
+from flask_bcrypt import Bcrypt
+
+DATABASE = "C:/Users/19171/PycharmProjects/OnlineTeReoDictionary/TeReoDictionary/maindictionary.db"
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.secret_key = "QLGus49&"
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return(connection)
+    except Error as e:
+        print(e)
+    return None
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('mainpage.html')
+
+
+if __name__ == '__main__':
+    app.run()
