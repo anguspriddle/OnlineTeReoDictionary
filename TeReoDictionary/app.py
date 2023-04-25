4b557c5e7bf9f0ba34625e4e5884f5465f8c3a77
gus <19171@wc.school.nz>
2023-04-26 07:32:12 +1200

    Created search page, added full visual dictionary to full dictionary page

--- TeReoDictionary/app.py
+++ TeReoDictionary/app.py
@@ -22,7 +22,39 @@ def hello_world():  # put application's code here
 
 @app.route('/full')
 def render_fulldictionary():
+    con = create_connection(DATABASE)
+    query = "SELECT Maori, English, Category, Definition, YearLevel FROM FullDictionary"
+    cur = con.cursor()
+    cur.execute(query)
+    dictionary = cur.fetchall()
+    con.close()
+    print(dictionary)
+    return render_template('full.html', fulldictionary=dictionary)
+
+@app.route('/full/<cat_id>')
+def render_fulldictionary_categories(cat_id):
+    con = create_connection(DATABASE)
+    query = "SELECT Maori, English, Category, Definition, YearLevel FROM FullDictionary WHERE cat_id=?"
+    cur = con.cursor()
+    cur.execute(query, (cat_id,))
+    dictionary = cur.fetchall()
+    con.close()
+    print(dictionary)
     return render_template('full.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def render_search():
+    search = request.form['search']
+    title = "Search for " + search
+    con = create_connection(DATABASE)
+    query = "SELECT Maori, English, Category, Definition YearLevel FROM FullDictionary WHERE " \
+            "Maori like ? OR English like ? OR Category like ? OR Definition like ? OR YearLevel like ?"
+    search = "%" + search + "%"
+    cur = con.cursor()
+    cur.execute(query, (search, search, search, search, search))
+    dictionary = cur.fetchall()
+    con.close()
+    return render_template("search.html", searchdictionary = dictionary, title=title)
 
 if __name__ == '__main__':
     app.run()
