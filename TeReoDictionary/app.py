7912899b7d604164ecc12d515a7f014ee4168d24
gus <19171@wc.school.nz>
2023-04-28 14:06:49 +1200

    attempted to redo catgeories

--- TeReoDictionary/app.py
+++ TeReoDictionary/app.py
@@ -3,8 +3,8 @@ import sqlite3
 from sqlite3 import Error
 from flask_bcrypt import Bcrypt
 
-# DATABASE = "C:/Users/19171/PycharmProjects/OnlineTeReoDictionary/TeReoDictionary/maindictionary.db"
-DATABASE = "D:/13dts/OnlineTeReoDictionary/TeReoDictionary/maindictionary.db"
+DATABASE = "C:/Users/19171/PycharmProjects/OnlineTeReoDictionary/TeReoDictionary/maindictionary.db"
+# DATABASE = "D:/13dts/OnlineTeReoDictionary/TeReoDictionary/maindictionary.db"
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.secret_key = "QLGus49&"
@@ -31,16 +31,20 @@ def render_fulldictionary():
     print(dictionary)
     return render_template('full.html', fulldictionary=dictionary)
 
-@app.route('/full/<cat_id>')
-def render_fulldictionary_categories(cat_id):
+@app.route('/full/<Category>')
+def render_fulldictionary_categories(Category):
     con = create_connection(DATABASE)
-    query = "SELECT Maori, English, Category, Definition, YearLevel FROM FullDictionary WHERE cat_id=?"
+    query = "SELECT Maori, English, Category, Definition, YearLevel FROM FullDictionary WHERE Category=?"
     cur = con.cursor()
-    cur.execute(query, (cat_id,))
+    cur.execute(query, (Category,))
+    query = "SELECT id, Category FROM Categories"
+    cur = con.cursor()
+    cur.execute(query)
+    category_list = cur.fetchall()
     dictionary = cur.fetchall()
     con.close()
     print(dictionary)
-    return render_template('full.html')
+    return render_template('full.html', dictionary=dictionary, category_list=category_list)
 
 @app.route('/search', methods=['GET', 'POST'])
 def render_search():
